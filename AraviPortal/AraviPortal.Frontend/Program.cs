0fe8811bd7d5d83e068a850b0758142a135d09c8
jrhenry40 <jrhenry40@hotmail.com>
2025-06-11 13:28:57 -0500

    Add page city create (010)

--- AraviPortal/AraviPortal.Frontend/Program.cs
+++ AraviPortal/AraviPortal.Frontend/Program.cs
@@ -1,5 +1,6 @@
 using AraviPortal.Frontend;
 using AraviPortal.Frontend.Repositories;
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -10,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7190") });
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddLocalization();
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
