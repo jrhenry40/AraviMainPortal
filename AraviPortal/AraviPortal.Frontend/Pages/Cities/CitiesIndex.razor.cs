3dc6b2a50267b3b52406d0070ff596c16810314a
jrhenry40 <jrhenry40@hotmail.com>
2025-06-11 13:59:35 -0500

    Edit and delete cities

--- AraviPortal/AraviPortal.Frontend/Pages/Cities/CitiesIndex.razor.cs
+++ AraviPortal/AraviPortal.Frontend/Pages/Cities/CitiesIndex.razor.cs
@@ -1,6 +1,7 @@
 using AraviPortal.Frontend.Repositories;
 using AraviPortal.Shared.Entities;
 using AraviPortal.Shared.Resources;
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Localization;
 
@@ -9,14 +10,71 @@ namespace AraviPortal.Frontend.Pages.Cities;
 public partial class CitiesIndex
 {
     [Inject] private IRepository Repository { get; set; } = null!;
-
     [Inject] private IStringLocalizer<Literals> Localizer { get; set; } = null!;
+    [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+    [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
 
     private List<City>? Cities { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
+        await LoadAsync();
+    }
+
+    private async Task LoadAsync()
+    {
         var responseHppt = await Repository.GetAsync<List<City>>("api/cities");
+        if (responseHppt.Error)
+        {
+            var message = await responseHppt.GetErrorMessageAsync();
+            await SweetAlertService.FireAsync(Localizer["Error"], message, SweetAlertIcon.Error);
+            return;
+        }
         Cities = responseHppt.Response!;
+    }
+
+    private async Task DeleteAsync(City city)
+    {
+        var result = await SweetAlertService.FireAsync(new SweetAlertOptions
+        {
+            Title = Localizer["Confirmation"],
+            Text = string.Format(Localizer["DeleteConfirm"], Localizer["City"], city.Name),
+            Icon = SweetAlertIcon.Question,
+            ShowCancelButton = true,
+            CancelButtonText = Localizer["Cancel"]
+        });
+
+        var confirm = string.IsNullOrEmpty(result.Value);
+
+        if (confirm)
+        {
+            return;
+        }
+
+        var responseHttp = await Repository.DeleteAsync($"api/cities/{city.Id}");
+        if (responseHttp.Error)
+        {
+            if (responseHttp.HttpResponseMessage.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                NavigationManager.NavigateTo("/");
+            }
+            else
+            {
+                var mensajeError = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync(Localizer["Error"], mensajeError, SweetAlertIcon.Error);
+            }
+            return;
+        }
+
+        await LoadAsync();
+        var toast = SweetAlertService.Mixin(new SweetAlertOptions
+        {
+            Toast = true,
+            Position = SweetAlertPosition.BottomEnd,
+            ShowConfirmButton = true,
+            Timer = 3000,
+            ConfirmButtonText = Localizer["Yes"]
+        });
+        toast?.FireAsync(icon: SweetAlertIcon.Success, message: Localizer["RecordDeletedOk"]);
     }
 }
---
+++ AraviPortal/AraviPortal.Frontend/Pages/Cities/CityEdit.razor.cs
@@ -0,0 +1,71 @@
+using AraviPortal.Frontend.Repositories;
+using AraviPortal.Shared.Entities;
+using AraviPortal.Shared.Resources;
+using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+
+namespace AraviPortal.Frontend.Pages.Cities;
+
+public partial class CityEdit
+{
+    private City? city;
+    private CityForm? cityForm;
+
+    [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+    [Inject] private IRepository Repository { get; set; } = null!;
+    [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+    [Inject] private IStringLocalizer<Literals> Localizer { get; set; } = null!;
+
+    [Parameter] public int Id { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        var responseHttp = await Repository.GetAsync<City>($"api/cities/{Id}");
+
+        if (responseHttp.Error)
+        {
+            if (responseHttp.HttpResponseMessage.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                NavigationManager.NavigateTo("cities");
+            }
+            else
+            {
+                var messageError = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync(Localizer["Error"], messageError, SweetAlertIcon.Error);
+            }
+        }
+        else
+        {
+            city = responseHttp.Response;
+        }
+    }
+
+    private async Task EditAsync()
+    {
+        var responseHttp = await Repository.PutAsync("api/cities", city);
+
+        if (responseHttp.Error)
+        {
+            var mensajeError = await responseHttp.GetErrorMessageAsync();
+            await SweetAlertService.FireAsync(Localizer["Error"], mensajeError, SweetAlertIcon.Error);
+            return;
+        }
+
+        Return();
+        var toast = SweetAlertService.Mixin(new SweetAlertOptions
+        {
+            Toast = true,
+            Position = SweetAlertPosition.BottomEnd,
+            ShowConfirmButton = true,
+            Timer = 3000
+        });
+        await toast.FireAsync(icon: SweetAlertIcon.Success, message: Localizer["RecordSavedOk"]);
+    }
+
+    private void Return()
+    {
+        cityForm!.FormPostedSuccessfully = true;
+        NavigationManager.NavigateTo("cities");
+    }
+}
