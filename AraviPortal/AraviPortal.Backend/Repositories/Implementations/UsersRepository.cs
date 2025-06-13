c756ab6bb70eddcfed7777967b286a05d959521d
jrhenry40 <jrhenry40@hotmail.com>
2025-06-13 13:48:51 -0500

    Add confirm users

--- AraviPortal/AraviPortal.Backend/Repositories/Implementations/UsersRepository.cs
+++ AraviPortal/AraviPortal.Backend/Repositories/Implementations/UsersRepository.cs
@@ -44,6 +44,24 @@ public class UsersRepository : IUsersRepository
         }
     }
 
+    public async Task<User> GetUserAsync(Guid userId)
+    {
+        var user = await _context.Users
+            .Include(u => u.City)
+            .FirstOrDefaultAsync(x => x.Id == userId.ToString());
+        return user!;
+    }
+
+    public async Task<string> GenerateEmailConfirmationTokenAsync(User user)
+    {
+        return await _userManager.GenerateEmailConfirmationTokenAsync(user);
+    }
+
+    public async Task<IdentityResult> ConfirmEmailAsync(User user, string token)
+    {
+        return await _userManager.ConfirmEmailAsync(user, token);
+    }
+
     public async Task<User> GetUserAsync(string email)
     {
         var user = await _context.Users
@@ -59,7 +77,7 @@ public class UsersRepository : IUsersRepository
 
     public async Task<SignInResult> LoginAsync(LoginDTO model)
     {
-        return await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+        return await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, true);
     }
 
     public async Task LogoutAsync()
